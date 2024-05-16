35674494a2b14591f9df59fb4888d46ad932ca51
Snezhi <snezhana_petrova@gmail.com>
2024-05-16 13:32:23 +0300

    models

--- Virtual Wallet/data_/models.py
+++ Virtual Wallet/data_/models.py
@@ -1,9 +1,8 @@
-from typing import Optional, List
-from fastapi import Query
+
 from pydantic import BaseModel, EmailStr, Field
 from datetime import date, datetime
+import numpy as np
 
-from data_.database import read_query
 
 
 class Role:
@@ -12,7 +11,7 @@ class Role:
     USER = 'user'
 
 
-class STATUS:
+class Status:
 
     PENDING = 'pending'
     CONFIRMED = 'confirmed'
@@ -29,19 +28,18 @@ class User(BaseModel):
     phone_number: str = Field(max_length=10)
     role: str = Field(default=Role.USER, description="User role, e.g., 'admin', 'user'")
     hashed_password: str | None = None
-    disabled: bool | None = None
     is_blocked: bool = Field(default=False)
-
+    disabled: bool | None = None
 
     @classmethod
     def from_query_result(cls, id: int, username: str, first_name: str, last_name: str, email: str,
-                          date_of_birth: date, hashed_password,role):
+                          phone_number: str, hashed_password,role):
         return cls(id=id,
                    username=username,
                    first_name=first_name,
                    last_name=last_name,
                    email=email,
-                   date_of_birth=date_of_birth,
+                   phone_number=phone_number,
                    hashed_password=hashed_password,
                    role=role)
 
@@ -65,3 +63,78 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     username: str | None = None
+
+
+class Cards(BaseModel):
+
+    id: int|None = None
+    number: str = Field(length=16)
+    expiration_date: date = np.datetime64('2022') + np.timedelta64(5,'Y')   ####TODO
+    cardholder_id: int
+    cvv: int = Field(length=3)
+
+@classmethod
+def from_query_result(cls, id: int, number: str, expiration_date:date, cardholder_id: int, cvv:int):
+    return cls(id=id,
+               number=number,
+               expiration_date=expiration_date,
+               cardholder_id=cardholder_id,
+               cvv=cvv)
+
+class Transactions(BaseModel):
+
+    id: int|None = None
+    is_recurring: bool = Field(default=False)
+    amount: float
+    status: str|None = None
+    message:str|None = None
+
+@classmethod
+def from_query_result(cls, id: int, is_recurring: bool, amount:float, status: str, message:str):
+    return cls(id=id,
+               is_recurring=is_recurring,
+               amount=amount,
+               status=status,
+               message=message)
+
+
+class Wallet(BaseModel):
+
+    id: int|None = None
+    amount: float|None
+    user_id: int
+
+class TransactionHistory(BaseModel):
+
+    user_id: int
+    transaction_id: int|None = None
+    recurring_date: date = datetime.now()  # !
+
+    @classmethod
+    def from_query_result(cls, user_id: int, transaction_id: int, recurring_date:date):
+        return cls(id=id,
+                   user_id=user_id,
+                   transaction_id=transaction_id,
+                   recurring_date=recurring_date)
+
+class ContactList(BaseModel):
+
+    id: int|None = None
+    user_id: int
+    contact_id: int
+    amount_sent: float
+    amount_received: float
+
+    @classmethod
+    def from_query_result(cls, user_id: int, contact_id: int, amount_sent:date, amount_received: float):
+        return cls(id=id,
+                   user_id=user_id,
+                   contact_id=contact_id,
+                   amount_sent=amount_sent,
+                   amount_received=amount_received)
+
+class Categories(BaseModel):
+
+    id: int|None = None
+    title: str
+    transaction_id: int
