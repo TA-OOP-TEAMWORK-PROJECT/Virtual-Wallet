ee514f0dad15243bffc070b984c89931c423fcd7
s-hristov91 <s.hristov.arch@gmail.com>
2024-05-16 12:25:04 +0300

    updates

--- Virtual Wallet/data_/database.py
+++ Virtual Wallet/data_/database.py
@@ -5,7 +5,7 @@ from mariadb.connections import Connection
 def _get_connection() -> Connection: #
     return connect(
         user='root',
-        password='b0t1ick0',
+        password='your_password',
         host='localhost',
         port=3306,
         database='virtual_wallet'
