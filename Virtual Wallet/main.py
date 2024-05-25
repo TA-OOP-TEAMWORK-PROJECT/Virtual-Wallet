1781a82cb087e69ada2f6176a2bb607ec6ea9dbf
s-hristov91 <s.hristov.arch@gmail.com>
2024-05-25 19:16:01 +0300

    final model update config

--- Virtual Wallet/main.py
+++ Virtual Wallet/main.py
@@ -7,6 +7,8 @@ from routers.transaction_router import transaction_router
 from routers.user_router import user_router
 from routers.category_router import category_router
 from routers.finance_router import finance_router
+from routers.card_router import card_router
+from routers.contact_router import contact_router
 
 app = FastAPI(title="PayTheBills", description=description, openapi_tags=tags_metadata)
 
@@ -17,6 +19,8 @@ app.include_router(transaction_router)
 app.include_router(wallet_router)
 app.include_router(category_router)
 app.include_router(finance_router)
+app.include_router(card_router)
+app.include_router(contact_router)
 
 if __name__ == '__main__':
     uvicorn.run(app='main:app', host='127.0.0.1', port=8001)
